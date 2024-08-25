962aaeecd9aba8736290ce963a9277586eecbc20
Maeganbcode <hairstylistmae@gmail.com>
2024-08-24 21:52:28 -0500

    orbitReport-noCss

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,16 @@
-const Buttons = ({ filterByType, setSat, displaySats }) => {
-  return (
+import satData from "./satData";
+
+const Buttons = ({ filterByType, setSat, orbitTypes }) => {
+    return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+      {orbitTypes.map((type, index) => (      
+      <button onClick={() => filterByType(type)} key={index}>
+        {type} Orbit
+      </button> 
+      ))}      
+      <button onClick={() => setSat(satData)}>  
+        All Orbits      
+      </button>
     </div>
   );
 };
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,18 +1,30 @@
 const Table = ({ sat }) => {
   return (
     <table>
-    <thead>
-     <tr>
-       <th>Header TBD</th>
-     </tr>
-     </thead>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
+        </tr>
+      </thead>
      <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
+     {sat.map((data) => (
+        <tr key={data.id}> 
+          <td>{data.name}</td>
+          <td>{data.type}</td>
+          <td>{data.launchDate}</td>
+          <td>{data.operational ? "Active" : "Inactive"}
+          </td>
+        </tr>
+     ))}
      </tbody>
    </table>
   );
 };
+                
+    
+
 
 export default Table;
